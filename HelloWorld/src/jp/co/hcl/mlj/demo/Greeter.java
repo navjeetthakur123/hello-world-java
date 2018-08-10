47845ef41de69d2364eba23e5c13c03b38aa2a47
navjeet.thakur <navjeet.thakur@LP-7FR4ZM2.GEO.CORP.HCL.IN>
2018-08-10 17:37:33 +0900

    Initial Commit

---
+++ HelloWorld/src/jp/co/hcl/mlj/demo/Greeter.java
@@ -0,0 +1,9 @@
+package jp.co.hcl.mlj.demo;
+
+public class Greeter {
+	
+	    public String sayHello() {
+	        return "Hello world!";
+	    }
+	}
+
---
+++ HelloWorld/src/jp/co/hcl/mlj/demo/HelloWorld.java
@@ -0,0 +1,12 @@
+package jp.co.hcl.mlj.demo;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Greeter greeter = new Greeter();
+	        System.out.println(greeter.sayHello());
+
+	}
+
+}
