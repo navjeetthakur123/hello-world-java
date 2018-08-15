06c1696d4068dec7b8190e23cb5d31b13d7e3df5
navjeet.thakur <navjeet.thakur@LP-7FR4ZM2.GEO.CORP.HCL.IN>
2018-08-15 12:35:07 +0900

    Modified throw clause

--- HelloWorld/src/jp/co/hcl/mlj/demo/HelloWorld.java
+++ HelloWorld/src/jp/co/hcl/mlj/demo/HelloWorld.java
@@ -2,7 +2,8 @@ package jp.co.hcl.mlj.demo;
 
 public class HelloWorld {
 
-	public static void main(String[] args) throws  Exception{
+	public static void main(String[] args) //throws  Exception{
+	{
 		// TODO Auto-generated method stub
 		 Greeter greeter = new Greeter();
 	        System.out.println(greeter.sayHello());
