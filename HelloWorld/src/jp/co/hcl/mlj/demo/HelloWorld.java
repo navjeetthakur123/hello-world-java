5677c87e623898da07853bc156569f6bb76daee4
navjeet.thakur <navjeet.thakur@LP-7FR4ZM2.GEO.CORP.HCL.IN>
2018-08-16 15:46:50 +0900

    Removed bugs

--- HelloWorld/src/jp/co/hcl/mlj/demo/HelloWorld.java
+++ HelloWorld/src/jp/co/hcl/mlj/demo/HelloWorld.java
@@ -4,11 +4,11 @@ import java.math.BigDecimal;
 
 public class HelloWorld {
 
-	public static void main(String[] args) throws  Exception{
-	//{
+	public static void main(String[] args) //throws  Exception{
+	{
 		// TODO Auto-generated method stub
-		double d = 1.1;
-		BigDecimal bd1 = new BigDecimal(d); 
+		//double d = 1.1;
+		//BigDecimal bd1 = new BigDecimal(d); 
 		 Greeter greeter = new Greeter();
 	        System.out.println(greeter.sayHello());
 	        if(true) {}
