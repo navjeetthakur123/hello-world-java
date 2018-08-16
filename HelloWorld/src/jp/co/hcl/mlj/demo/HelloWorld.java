e178621cb916bc3bc8e2a4ba1bf11b499c31ba82
navjeet.thakur <navjeet.thakur@LP-7FR4ZM2.GEO.CORP.HCL.IN>
2018-08-16 12:46:45 +0900

    Added Bug

--- HelloWorld/src/jp/co/hcl/mlj/demo/HelloWorld.java
+++ HelloWorld/src/jp/co/hcl/mlj/demo/HelloWorld.java
@@ -1,10 +1,14 @@
 package jp.co.hcl.mlj.demo;
 
+import java.math.BigDecimal;
+
 public class HelloWorld {
 
 	public static void main(String[] args) //throws  Exception{
 	{
 		// TODO Auto-generated method stub
+		double d = 1.1;
+		BigDecimal bd1 = new BigDecimal(d); 
 		 Greeter greeter = new Greeter();
 	        System.out.println(greeter.sayHello());
 	        if(true) {}
